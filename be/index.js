2486af3dc87f4305aea061fa8c974c6f088c8309
Eakkarunpoom Tanyianniti <eakkarunpoomtan@gosoft.co.th>
2023-07-23 23:02:57 +0700

    first commit

---
+++ be/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
+const UserModel = require('./models/UserSchema');
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+const connectMongoose = process.env.dbmongoose
+
+const salt = bcrypt.genSaltSync(10);
+const port = 8080;
+mongoose.connect(connectMongoose)
+
+
+app.post('/signup', async (req,res)=> {
+  const email = req.body.email;
+  const password = req.body.password;
+  const user = await UserModel.create({email,password:bcrypt.hashSync(password,salt)})
+  res.json(user)
+})
+
+app.post('/login', (req,res) => {
+  res.json('login')
+})
+
+app.listen(port, ()=>{
+  console.log(`start ${port}`)
+})
